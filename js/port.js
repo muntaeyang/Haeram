79b9e4a31db28497995295a17fee788cd71de620
muntaeyang <plus96mjs@naver.com>
2023-11-28 22:38:30 +0900

    베타 8차 수정 포트폴리오 js

--- js/port.js
+++ js/port.js
@@ -13,3 +13,47 @@
 });
 
 
+const category1 = document.querySelector(".category .category1");
+const category2 = document.querySelector(".category .category2");
+const category3 = document.querySelector(".category .category3");
+const category4 = document.querySelector(".category .category4");
+
+const homepage = document.querySelector(".portpolio__items .homepage");
+const mobileapp = document.querySelector(".portpolio__items .mobileapp");
+const webprogram = document.querySelector(".portpolio__items .webprogram");
+const others = document.querySelector(".portpolio__items .others");
+
+const dropdownContent = document.querySelector(".portpolio__items");
+const drop_icon1 = document.querySelector(".portpolio__items .item");
+  
+category1.addEventListener("click", function() {
+  if (homepage.style.display = "flex") {
+      mobileapp.style.display = "none";
+      webprogram.style.display = "none";
+      others.style.display = "none";
+  } 
+});
+
+category2.addEventListener("click", function() {
+  if (mobileapp.style.display = "flex") {
+      homepage.style.display = "none";
+      webprogram.style.display = "none";
+      others.style.display = "none";
+  } 
+});
+
+category3.addEventListener("click", function() {
+  if (webprogram.style.display = "flex") {
+      homepage.style.display = "none";
+      mobileapp.style.display = "none";
+      others.style.display = "none";
+  } 
+});
+
+category4.addEventListener("click", function() {
+  if (others.style.display = "flex") {
+      homepage.style.display = "none";
+      mobileapp.style.display = "none";
+      webprogram.style.display = "none";
+  } 
+});
